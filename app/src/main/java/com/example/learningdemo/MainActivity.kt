b0e0c6d0286b4b8aa48473d7baeeed6e23c4cf88
JalalJanKhan <jalaljankhan@outlook.com>
2022-07-24 23:30:52 +0500

    Initial commit

---
+++ app/src/main/java/com/example/learningdemo/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.learningdemo
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.learningdemo.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.btnLogin.setOnClickListener {
+            var isFormFilled = true
+            val email = mBinding.etEmail.text.toString().trim()
+
+            if (email.isEmpty()) {
+                mBinding.etEmail.error = "Email is missing!"
+                isFormFilled = false
+            } else
+                mBinding.etEmail.error = null
+
+            val password = mBinding.etPassword.text.toString().trim()
+
+            if (password.isEmpty()) {
+                mBinding.etPassword.error = "Password is missing!"
+                isFormFilled = false
+            } else
+                mBinding.etPassword.error = null
+
+            if (isFormFilled) {
+                // Authenticate User
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnSuccessListener { authResult ->
+                        // Cache User's credentials
+                        val pref = getSharedPreferences("learning_demo", Context.MODE_PRIVATE)
+                        pref
+                            .edit()
+                            .putString("key_uid", authResult.user?.uid)
+                            .putString("key_email", email)
+                            .putString("key_password", password)
+                            .apply()
+                    }
+                    .addOnFailureListener { exception ->
+                        // Log exception into a file
+                        val file = File("logs.txt")
+                        file.appendText(
+                            text = exception.message.toString()
+                        )
+                    }
+            }
+        }
+    }
+
+}
