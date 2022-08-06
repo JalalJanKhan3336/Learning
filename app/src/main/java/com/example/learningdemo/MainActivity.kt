f9a3252d91f04623a631bddfbbf11feb763bd571
JalalJanKhan <jalaljankhan@outlook.com>
2022-08-06 15:47:13 +0500

    Jalal: Learning Dagger2

--- app/src/main/java/com/example/learningdemo/MainActivity.kt
+++ app/src/main/java/com/example/learningdemo/MainActivity.kt
@@ -15,47 +15,6 @@ class MainActivity : AppCompatActivity() {
 
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
-
-        mBinding.btnLogin.setOnClickListener {
-            var isFormFilled = true
-            val email = mBinding.etEmail.text.toString().trim()
-
-            if (email.isEmpty()) {
-                mBinding.etEmail.error = "Email is missing!"
-                isFormFilled = false
-            } else
-                mBinding.etEmail.error = null
-
-            val password = mBinding.etPassword.text.toString().trim()
-
-            if (password.isEmpty()) {
-                mBinding.etPassword.error = "Password is missing!"
-                isFormFilled = false
-            } else
-                mBinding.etPassword.error = null
-
-            if (isFormFilled) {
-                // Authenticate User
-                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                    .addOnSuccessListener { authResult ->
-                        // Cache User's credentials
-                        val pref = getSharedPreferences("learning_demo", Context.MODE_PRIVATE)
-                        pref
-                            .edit()
-                            .putString("key_uid", authResult.user?.uid)
-                            .putString("key_email", email)
-                            .putString("key_password", password)
-                            .apply()
-                    }
-                    .addOnFailureListener { exception ->
-                        // Log exception into a file
-                        val file = File("logs.txt")
-                        file.appendText(
-                            text = exception.message.toString()
-                        )
-                    }
-            }
-        }
     }
 
 }
